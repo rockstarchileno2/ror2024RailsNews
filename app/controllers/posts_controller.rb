517d6eeb885fc9d0013c934e1434b0ce0490f90a
vagrant <vagrant@ubuntu-server>
2024-08-26 11:46:22 +0000

    Arreglando los roles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[ show edit update destroy ]
-  before_action only: [:new, :create] do
-    authorize_request(["author", "admin"])
-  end
+  # before_action only: [:new, :create] do
+  #   authorize_request(["author", "admin"])
+  # end
   before_action only: [:edit, :update, :destroy] do
     authorize_request(["admin"])
   end
